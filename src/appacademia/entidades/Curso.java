9b73bd58f0ff60989485af3f6f9d1c50dae8c300
Kristian Johansson <carlkristian.johansson@gmail.com>
2022-11-04 13:55:27 +0100

    Base de datos centralizada

---
+++ src/appacademia/entidades/Curso.java
@@ -0,0 +1,247 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appacademia.entidades;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author usu2dam
+ */
+@Entity
+@Table(name = "CURSO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Curso.findAll", query = "SELECT c FROM Curso c"),
+    @NamedQuery(name = "Curso.findById", query = "SELECT c FROM Curso c WHERE c.id = :id"),
+    @NamedQuery(name = "Curso.findByNombre", query = "SELECT c FROM Curso c WHERE c.nombre = :nombre"),
+    @NamedQuery(name = "Curso.findByCategoria", query = "SELECT c FROM Curso c WHERE c.categoria = :categoria"),
+    @NamedQuery(name = "Curso.findByDuracion", query = "SELECT c FROM Curso c WHERE c.duracion = :duracion"),
+    @NamedQuery(name = "Curso.findByCertificacion", query = "SELECT c FROM Curso c WHERE c.certificacion = :certificacion"),
+    @NamedQuery(name = "Curso.findByFechaInicio", query = "SELECT c FROM Curso c WHERE c.fechaInicio = :fechaInicio"),
+    @NamedQuery(name = "Curso.findByFechaFin", query = "SELECT c FROM Curso c WHERE c.fechaFin = :fechaFin"),
+    @NamedQuery(name = "Curso.findByProveedor", query = "SELECT c FROM Curso c WHERE c.proveedor = :proveedor"),
+    @NamedQuery(name = "Curso.findByNumAsistentes", query = "SELECT c FROM Curso c WHERE c.numAsistentes = :numAsistentes"),
+    @NamedQuery(name = "Curso.findByTipo", query = "SELECT c FROM Curso c WHERE c.tipo = :tipo"),
+    @NamedQuery(name = "Curso.findByBeca", query = "SELECT c FROM Curso c WHERE c.beca = :beca"),
+    @NamedQuery(name = "Curso.findByImporte", query = "SELECT c FROM Curso c WHERE c.importe = :importe")})
+public class Curso implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "NOMBRE")
+    private String nombre;
+    @Basic(optional = false)
+    @Column(name = "CATEGORIA")
+    private String categoria;
+    @Basic(optional = false)
+    @Column(name = "DURACION")
+    private int duracion;
+    @Basic(optional = false)
+    @Column(name = "CERTIFICACION")
+    private String certificacion;
+    @Basic(optional = false)
+    @Column(name = "FECHA_INICIO")
+    @Temporal(TemporalType.DATE)
+    private Date fechaInicio;
+    @Basic(optional = false)
+    @Column(name = "FECHA_FIN")
+    @Temporal(TemporalType.DATE)
+    private Date fechaFin;
+    @Basic(optional = false)
+    @Column(name = "PROVEEDOR")
+    private String proveedor;
+    @Basic(optional = false)
+    @Column(name = "NUM_ASISTENTES")
+    private int numAsistentes;
+    @Basic(optional = false)
+    @Column(name = "TIPO")
+    private String tipo;
+    @Basic(optional = false)
+    @Column(name = "BECA")
+    private Boolean beca;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Basic(optional = false)
+    @Column(name = "IMPORTE")
+    private BigDecimal importe;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "curso")
+    private Collection<Matricula> matriculaCollection;
+
+    public Curso() {
+    }
+
+    public Curso(Integer id) {
+        this.id = id;
+    }
+
+    public Curso(Integer id, String nombre, String categoria, int duracion, String certificacion, Date fechaInicio, Date fechaFin, String proveedor, int numAsistentes, String tipo, Boolean beca, BigDecimal importe) {
+        this.id = id;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.duracion = duracion;
+        this.certificacion = certificacion;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.proveedor = proveedor;
+        this.numAsistentes = numAsistentes;
+        this.tipo = tipo;
+        this.beca = beca;
+        this.importe = importe;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getCertificacion() {
+        return certificacion;
+    }
+
+    public void setCertificacion(String certificacion) {
+        this.certificacion = certificacion;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public int getNumAsistentes() {
+        return numAsistentes;
+    }
+
+    public void setNumAsistentes(int numAsistentes) {
+        this.numAsistentes = numAsistentes;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Boolean getBeca() {
+        return beca;
+    }
+
+    public void setBeca(Boolean beca) {
+        this.beca = beca;
+    }
+
+    public BigDecimal getImporte() {
+        return importe;
+    }
+
+    public void setImporte(BigDecimal importe) {
+        this.importe = importe;
+    }
+
+    @XmlTransient
+    public Collection<Matricula> getMatriculaCollection() {
+        return matriculaCollection;
+    }
+
+    public void setMatriculaCollection(Collection<Matricula> matriculaCollection) {
+        this.matriculaCollection = matriculaCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Curso)) {
+            return false;
+        }
+        Curso other = (Curso) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "appacademia.entidades.Curso[ id=" + id + " ]";
+    }
+    
+}
